bf2f20df556d6753cbf5b9402a3324baf5ce5bb6
Martin Page <martin.page@tsl.ac.uk>
2013-12-16 17:37:41 +0000

    css tweeks

--- src/frontend/cheeves.js
+++ src/frontend/cheeves.js
@@ -3,9 +3,9 @@ $( document ).ready(function() {
 	$('body').append('<div id="cheeve" class="hide-right"></div>');
 	$('#cheeve').append('<div id="left"></div>');
 	$('#cheeve').append('<div id="right"></div>');
-	$('#left').append('<img width="90" height="90" src="image.png" >');
+	$('#left').append('<img width="80" height="80" src="image.png" >');
 	$('#right').append('<h2>Feature Editor 1/5</h2>');
-	$('#right').append('<p>Edit 4 more features to earn this badge</p>');
+	$('#right').append('<p>Edit 4 more features to earn this</p>');
 
 
 	$('#cheeve').click(function(event) {
